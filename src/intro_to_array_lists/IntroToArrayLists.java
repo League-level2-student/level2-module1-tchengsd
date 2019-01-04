628deba92328df50052a807bbd9893f1fb19011f
league <league@worthymini-5.attlocal.net>
2019-01-03 19:55:13 -0800

    Intro to Array Lists

--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,45 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
-		
-		//2. Add five Strings to your list
-		
-		//3. Print all the Strings using a standard for-loop
-		
-		//4. Print all the Strings using a for-each loop
-		
-		//5. Print only the even numbered elements in the list.
-		
-		//6. Print all the Strings in reverse order.
-		
-		//7. Print only the Strings that have the letter 'e' in them.
+		// 1. Create an array list of Strings
+		// Don't forget to import the ArrayList class
+		ArrayList<String> phrases = new ArrayList<String>();
+		// 2. Add five Strings to your list
+		phrases.add("Hello.");
+		phrases.add("Is this thing on?");
+		phrases.add("I like spaghetti.");
+		phrases.add("I don't like fourtnoyght.");
+		phrases.add("What do you like?");
+		// 3. Print all the Strings using a standard for-loop
+		for (int i = 0; i < phrases.size(); i++) {
+			String s = phrases.get(i);
+			System.out.println(s);
+		}
+		// 4. Print all the Strings using a for-each loop
+		for (String sentence : phrases) {
+			System.out.println(sentence);
+		}
+		// 5. Print only the even numbered elements in the list.
+		for (int i = 0; i < phrases.size(); i++) {
+			String s = phrases.get(i);
+			if (i % 2 == 0) {
+				System.out.println(s);
+			}
+		}
+		// 6. Print all the Strings in reverse order.
+		for (int i = phrases.size() - 1; i > -1; i--) {
+			String s = phrases.get(i);
+			System.out.println(s);
+		}
+		// 7. Print only the Strings that have the letter 'e' in them.
+		for (int i = 0; i < phrases.size(); i++) {
+			String s = phrases.get(i);
+			if(s.contains("e")) {
+				System.out.println(s);
+			}
+		}
 	}
 }
